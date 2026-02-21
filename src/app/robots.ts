9fc4e17ebb8b55035a5b7092b2408cf6e25eb1f1
harshitdhodi <harshit.dhodi2108@gmail.com>
2026-02-21 10:14:46 +0530

    update port number

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     // Pulls the domain from your env vars; provides a fallback for local dev
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3059';
 
     return {
         rules: {
